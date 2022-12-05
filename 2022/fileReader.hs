79ac90bbf9af1c5f26fd2bec628bf0bc0efca3fb
BunnyFiscuit <sarosh.nasir@gmail.com>
2022-12-05 13:56:52 +0100

    update fileReader

--- 2022/fileReader.hs
+++ 2022/fileReader.hs
@@ -7,7 +7,7 @@ readF n = do
   contents <- readFile ("inputs/" ++ show n ++ ".txt")
   return $ lines contents
 
-readFStr :: String -> IO [String]
-readFStr s = do
+readF' :: String -> IO [String]
+readF' s = do
   contents <- readFile ("inputs/" ++ s ++ ".txt")
   return $ lines contents
