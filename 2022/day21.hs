e8d520dac77697c39e44067e6fb862052d5aa05e
BunnyFiscuit <sarosh.nasir@gmail.com>
2022-12-22 00:27:16 +0100

    used Map for day 21

--- 2022/day21.hs
+++ 2022/day21.hs
@@ -4,22 +4,21 @@ import FileReader
 import Data.List
 import Data.Maybe
 import Text.Read
+import qualified Data.Map as M
 
 main :: IO ()
 main = do
   contents <- readF' "21"
-  let screams = map readLine contents
-  let root@(MOp m (S va) op (S vb)) = findRoot screams
-  print va
-  print vb
-  let rootValue = doOp op (getValue va 0 screams) (getValue vb 0 screams)
+  let screams = M.fromList (map readLine contents)
+  let root@(m1, op, m2) = findRoot screams
+  let rootValue = doOp op (getValue m1 screams) (getValue m2 screams)
   putStrLn $ "part 1: " ++ show rootValue
 
 data Op = Add | Sub | Mult | Div deriving (Show, Eq, Read)
-data Value = S String | I Int deriving (Show, Eq, Read)
-data Expr = MOp Monkey Value Op Value | MVal String Value  deriving (Show, Eq, Read)
-
 type Monkey = String
+
+type Screams = M.Map Monkey (Either Expr Int)
+type Expr = (String, Op, String)
 
 readOp :: String -> Op
 readOp "+" = Add
@@ -33,38 +32,23 @@ doOp Sub  a b = a - b
 doOp Mult a b = a * b
 doOp Div  a b = div a b
 
-readValue :: String -> Value
-readValue str = case readMaybe str :: Maybe Int of
-  (Just v) -> I v
-  Nothing  -> S str
-
-readLine :: String -> Expr
+readLine :: String -> (Monkey, Either Expr Int)
 readLine str = case length ws of
-  2 -> MVal monkey (readValue (ws !! 1))
-  4 -> MOp monkey (readValue (ws !! 1)) (readOp (ws !! 2)) (readValue (ws !! 3))
-  _ -> error "Should not happen"
+  2 -> (monkey, Right (read (ws !! 1) :: Int))
+  4 -> (monkey, Left (ws !! 1, readOp (ws !! 2), ws !! 3))
   where 
     ws = words str
     monkey = takeWhile (/=':') (head ws)
-    firstVal = readMaybe (ws !! 1) :: Maybe Int
     
-findRoot :: [Expr] -> Expr
-findRoot [] = error "No root"
-findRoot (x:xs)
-  | getMonkey x == "root" = x
-  | otherwise = findRoot xs
+findRoot :: Screams -> Expr
+findRoot xs = case M.lookup "root" xs of
+  Nothing  -> error "No root"
+  Just (Left  e)   -> e
+  Just (Right _)   -> error "Root lookup should not have value"
 
-getValue :: Monkey -> Int -> [Expr] -> Int
-getValue monkey n xs
-  | monkey == m = case current of
-  MOp s (S va) op (S vb) -> doOp op (getValue va 0 xs) (getValue vb 0 xs)
-  MVal s (I va) -> va
+getValue :: Monkey -> Screams -> Int
+getValue monkey xs = case M.lookup monkey xs of
+  Nothing -> error "No such monkey"
+  Just (Left (m1, op, m2))  -> doOp op (getValue m1 xs) (getValue m2 xs)
+  Just (Right v) -> v
 
-  | otherwise = getValue monkey (n+1) xs
-  where 
-    current = head $ take 1 $ drop n xs
-    m = getMonkey current
-
-getMonkey :: Expr -> Monkey
-getMonkey (MVal m _) = m
-getMonkey (MOp m _ _ _) = m
