1ab969f8b1bcb08d8cba1f8b90c8f287235ea911
BunnyFiscuit <sarosh.nasir@gmail.com>
2022-12-22 01:28:29 +0100

    did something idk day 21

--- 2022/day21.hs
+++ 2022/day21.hs
@@ -11,8 +11,13 @@ main = do
   contents <- readF' "21"
   let screams = M.fromList (map readLine contents)
   let root@(m1, op, m2) = findRoot screams
+  print m1
+  print m2
   let rootValue = doOp op (getValue m1 screams) (getValue m2 screams)
   putStrLn $ "part 1: " ++ show rootValue
+  let maxInt = maxBound :: Int
+  p2 <- part2 screams root 0 maxInt
+  putStrLn $ "part 2: " ++ show p2
 
 data Op = Add | Sub | Mult | Div deriving (Show, Eq, Read)
 type Monkey = String
@@ -36,10 +41,10 @@ readLine :: String -> (Monkey, Either Expr Int)
 readLine str = case length ws of
   2 -> (monkey, Right (read (ws !! 1) :: Int))
   4 -> (monkey, Left (ws !! 1, readOp (ws !! 2), ws !! 3))
-  where 
+  where
     ws = words str
     monkey = takeWhile (/=':') (head ws)
-    
+
 findRoot :: Screams -> Expr
 findRoot xs = case M.lookup "root" xs of
   Nothing  -> error "No root"
@@ -52,3 +57,14 @@ getValue monkey xs = case M.lookup monkey xs of
   Just (Left (m1, op, m2))  -> doOp op (getValue m1 xs) (getValue m2 xs)
   Just (Right v) -> v
 
+type Root = (String, Op, String)
+part2 :: Screams -> Root -> Int -> Int -> IO Int
+part2 xs (m1, op, m2) lo hi = do
+  let humn = (lo + hi) `div` 2
+  let xs' = M.update (\v -> Just (Right humn)) "humn" xs
+  let leftVal = getValue m1 xs'
+  let rightVal = getValue m2 xs'
+  let diff = leftVal - rightVal
+  if diff == 0 then return humn else 
+    if diff > 0 then part2 xs (m1, op, m2) humn hi
+    else part2 xs (m1, op, m2) lo humn
